221cf15dc4dfee4ad240d261ee9c109aca0b226d
Richard Cook <rcook@rcook.org>
2025-06-30 14:08:24 -0700

    Stricter check

--- src/bbc_basic/util.rs
+++ src/bbc_basic/util.rs
@@ -1,6 +1,7 @@
 use crate::bbc_basic::{END_MARKER, TOKEN_MASK};
+use crate::line_ending::CR;
 use anyhow::{Result, bail};
-use std::fs::File;
+use std::fs::{File, metadata};
 use std::io::{ErrorKind, Read, Seek, SeekFrom};
 use std::path::Path;
 
@@ -9,7 +10,19 @@ pub const fn is_token(value: u8) -> bool {
 }
 
 pub fn is_bbc_basic_file(content_path: &Path) -> Result<bool> {
+    let m = metadata(content_path)?;
+    if m.len() < 3 {
+        return Ok(false);
+    }
+
     let mut f = File::open(content_path)?;
+
+    let mut bytes = [0; 1];
+    f.read_exact(&mut bytes)?;
+    if bytes[0] != CR {
+        return Ok(false);
+    }
+
     match f.seek(SeekFrom::End(-2)) {
         Ok(_) => {}
         Err(e) if e.kind() == ErrorKind::InvalidInput => return Ok(false),
